eef3f47595d0b5f5f08bfdf1932b01c7f243c1d1
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-26 00:55:36 +0500

    Task6

---
+++ Tyuiu.BerezovskiyAS.Sprint5.Task6.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Tyuiu.BerezovskiyAS.Sprint5.Task6.V22.Lib;
+
+namespace Tyuiu.BerezovskiyAS.Sprint5.Task6.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\berez\AppData\Local\Temp\DataSprint5\InPutDataFileTask6V22.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExsists);
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint5.Task6.V22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Tyuiu.BerezovskiyAS.Sprint5.Task6.V22.Lib;
+
+{
+    DataService ds = new DataService();
+
+
+    Console.Title = "Спринт #5 | Выполнил: Березовский А. С. | ПИНб-25-1";
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #5                                                               *");
+    Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+    Console.WriteLine("* Задание #6                                                              *");
+    Console.WriteLine("* Вариант #22                                                             *");
+    Console.WriteLine("* Выполнил: Березовский Артём Станиславович | ПИНб-25-1                   *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Дан файл (файл взять из архива согласно вашему варианту. Создать папку  *");
+    Console.WriteLine("* в ручную и скопировать в неё файл) в котором есть набор символьных      *");
+    Console.WriteLine("* данных. Найти количество удвоенных букв мм в заданной строке.           *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    string path = @"C:\Users\berez\AppData\Local\Temp\DataSprint5\InPutDataFileTask6V22.txt";
+    Console.WriteLine("Данные находятся в файле: " + path);
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+
+
+    double res = ds.LoadFromDataFile(path);
+
+    Console.WriteLine(res);
+    Console.ReadKey();
+}
