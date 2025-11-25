35f6b8e2a6b5d487f2507004340e3fc40600e661
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-25 17:24:02 +0500

    Task3

---
+++ Tyuiu.BerezovskiyAS.Sprint5.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.BerezovskiyAS.Sprint5.Task3.V22.Lib
+{
+    public class DataService : ISprint5Task3V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string tempPath = Path.GetTempPath();
+            string fileName = "OutPutFileTask3.bin";
+            string filePath = Path.Combine(tempPath, fileName);
+
+            double y = Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3);
+
+            byte[] binData = BitConverter.GetBytes(y);
+
+            File.WriteAllBytes(filePath, binData);
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint5.Task3.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Tyuiu.BerezovskiyAS.Sprint5.Task3.V22.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint5.Task3.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+
+            string path = ds.SaveToFileTextData(3);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
