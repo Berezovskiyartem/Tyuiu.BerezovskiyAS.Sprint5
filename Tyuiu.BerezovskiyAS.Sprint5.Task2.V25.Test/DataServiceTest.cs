156ae8f6821c971e12841cc63a6957deef6e92d7
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-25 16:14:47 +0500

    Task2

--- Tyuiu.BerezovskiyAS.Sprint5.Task2.V25.Test/DataServiceTest.cs
+++ Tyuiu.BerezovskiyAS.Sprint5.Task2.V25.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.BerezovskiyAS.Sprint5.Task2.V25.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = $@"D:\source\repos\Tyuiu.BerezovskiyAS.Sprint5\Tyuiu.BerezovskiyAS.Sprint5.Task2.V25\bin\Debug\OutPutFileTask2.csv";
+            string path = $@"D:\source\repos\Tyuiu.BerezovskiyAS.Sprint5\Tyuiu.BerezovskiyAS.Sprint5.Task2.V25\bin\Debug\net8.0\OutPutFileTask2.csv";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
